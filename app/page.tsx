8064fd5d02a689a20f51f439af7b8edabf55894e
arielchama <arielchama45@gmail.com>
2024-09-20 20:00:17 +0100

    style: improving mobile design

--- app/page.tsx
+++ app/page.tsx
@@ -65,7 +65,7 @@ export default function Home() {
       </div>
 
       <main className={styles.main}>
-        <div>
+        <div className={styles.main_container}>
           <div className={styles.card}>
             <h3 className={styles.subtitle}>Suas tarefas de hoje</h3>
 
@@ -88,19 +88,19 @@ export default function Home() {
               <h3 className={styles.subtitle}>Tarefas finalizadas</h3>
 
               {tasks.filter((task) => task.completed).map((task) => (
-              <div className={styles.card_item}>
-                <div style={{ display: 'flex', gap: '16px', alignItems: 'center' }}>
-                  <input type="checkbox" checked={task.completed} onChange={() => toggleTaskCompletion(task.id)} />
-                  <p className={styles.card_task} style={{ textDecoration: 'line-through', color: '#0000008A' }}>{task.title}</p>
-                </div>
+                <div className={styles.card_item}>
+                  <div style={{ display: 'flex', gap: '16px', alignItems: 'center' }}>
+                    <input type="checkbox" checked={task.completed} onChange={() => toggleTaskCompletion(task.id)} />
+                    <p className={styles.card_task} style={{ textDecoration: 'line-through', color: '#0000008A' }}>{task.title}</p>
+                  </div>
 
-                <button type="button" className={styles.buttonDelete} onClick={() => handleModalDelete(task.id)}>
-                  <svg width="20" height="22" viewBox="0 0 20 22" fill="none">
-                    <path d="M1 5H3M3 5H19M3 5V19C3 19.5304 3.21071 20.0391 3.58579 20.4142C3.96086 20.7893 4.46957 21 5 21H15C15.5304 21 16.0391 20.7893 16.4142 20.4142C16.7893 20.0391 17 19.5304 17 19V5H3ZM6 5V3C6 2.46957 6.21071 1.96086 6.58579 1.58579C6.96086 1.21071 7.46957 1 8 1H12C12.5304 1 13.0391 1.21071 13.4142 1.58579C13.7893 1.96086 14 2.46957 14 3V5" stroke="#B0BBD1" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round" />
-                  </svg>
-                </button>
-              </div>
-            ))}
+                  <button type="button" className={styles.buttonDelete} onClick={() => handleModalDelete(task.id)}>
+                    <svg width="20" height="22" viewBox="0 0 20 22" fill="none">
+                      <path d="M1 5H3M3 5H19M3 5V19C3 19.5304 3.21071 20.0391 3.58579 20.4142C3.96086 20.7893 4.46957 21 5 21H15C15.5304 21 16.0391 20.7893 16.4142 20.4142C16.7893 20.0391 17 19.5304 17 19V5H3ZM6 5V3C6 2.46957 6.21071 1.96086 6.58579 1.58579C6.96086 1.21071 7.46957 1 8 1H12C12.5304 1 13.0391 1.21071 13.4142 1.58579C13.7893 1.96086 14 2.46957 14 3V5" stroke="#B0BBD1" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
+                  </button>
+                </div>
+              ))}
             </div>
           </div>
 
